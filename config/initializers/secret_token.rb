3c66b0a0402aee1e7164d6339cca2e45569471d5
Moiz K <moiz@heuretix.com>
2013-08-16 22:46:35 -0400

    Generate Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kremlin::Application.config.secret_token = '2377db0090166287cf27d3fa722273d003fbad0bf5e7bd0e56363980276a76a4a8af5861485d76a021d532c343094c52065fda8d84c4bdf90ac2865f00e0785a'
