9a24702d8d43b535c9ca12f331a07594c56405ed
Moiz K. Malik <moiz@heuretix.com>
2013-12-08 18:56:49 -0500

    Migration after Competitors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131207195158) do
+ActiveRecord::Schema.define(:version => 20131208233325) do
+
+  create_table "competitors", :force => true do |t|
+    t.string   "name"
+    t.string   "website"
+    t.string   "facebook"
+    t.string   "twitter"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "project_entries", :force => true do |t|
     t.integer  "project_id"
