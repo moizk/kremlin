dfbeaaee49240b9636d610a927f88ecc6ee24815
Moiz K. Malik <moiz@heuretix.com>
2013-12-09 10:22:00 -0500

    Improve People UI

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,8 @@
 class Person < ActiveRecord::Base
   attr_accessible :email, :facebook, :first_name, :last_name, :linkedin, :phone, :school_id, :title, :twitter
   belongs_to :school
+
+  def full_name
+  	first_name + last_name
+  end
 end
