67d31cc44b59f30f2f7ab90d7047758fb79cd779
Moiz K. Malik <moiz@heuretix.com>
2014-02-05 17:55:33 -0500

    Redo Note Categories Index Page to be Keyboard Centric

---
+++ app/assets/javascripts/filters.js
@@ -0,0 +1,17 @@
+$(function() {
+  $("#note_category_filter").focus();
+  $("#note_category_filter").on('keyup change', function() {
+    $("h5.note_category_title").each(function() {
+      var noteCategory, categorySearch;
+
+      categorySearch = $('#note_category_filter').val().toLowerCase() ? $('#note_category_filter').val().toLowerCase() : "";
+      noteCategory = $(this).data("name").toString().toLowerCase();
+
+      if (noteCategory.search(categorySearch)) {
+        $(this).hide();
+      } else {
+        $(this).show();
+      }
+    });
+  });
+});
